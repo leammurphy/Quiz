37f7991093b33a9f800827b34e4bdaa94a1872c7
leammurphy <leam.a.murphy@gmail.com>
2021-09-08 09:34:28 -0600

    Answer choices are now rendered on the page

--- quiz/src/components/QuestionBox.vue
+++ quiz/src/components/QuestionBox.vue
@@ -3,11 +3,10 @@
         <b-jumbotron header="Quiz" lead="Prepare Yourself">
             <template>
                 {{ currentQuestion.question }}
-                some questions
             </template>
             <hr class="my-4" />
-            <p>
-                list of answers
+            <p  v-for="(answer, index) in answers" :key="index">
+               {{answer}}
             </p>
             <b-button variant="primary" href="#">Submit</b-button>
             <b-button @click ="next" variant="success" href="#">Next</b-button>
@@ -21,6 +20,15 @@
             currentQuestion: Object,
             next: Function, 
         },
+        computed: {
+            answers() {
+               let answers =  [...this.currentQuestion.incorrect_answers]
+               answers.push(this.currentQuestion.correct_answer)
+               return answers
+            }
+        }
+
+
         // mounted() {
         //     console.log(this.currentQuestion)
         // }
