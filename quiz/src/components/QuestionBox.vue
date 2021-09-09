cdd3943bbc4f1c85522f05e167377224d7402297
leammurphy <leam.a.murphy@gmail.com>
2021-09-09 09:30:31 -0600

    Added the lodash library. Added css styling for better viewing. Added some functions to defferentiate between selected and non selected answers.

--- quiz/src/components/QuestionBox.vue
+++ quiz/src/components/QuestionBox.vue
@@ -5,9 +5,22 @@
                 {{ currentQuestion.question }}
             </template>
             <hr class="my-4" />
-            <p  v-for="(answer, index) in answers" :key="index">
-               {{answer}}
-            </p>
+
+                <b-list-group>
+                    <b-list-group-item 
+                        v-for="(answer, index) in answers" 
+                        :key="index"
+                        @click="selectAnswer(index)"
+                        :class="[selectedIndex === index ? 'selected' : '']"
+                    >
+                        
+                        {{answer}}
+                        
+                     </b-list-group-item>
+
+                </b-list-group>
+
+
             <b-button variant="primary" href="#">Submit</b-button>
             <b-button @click ="next" variant="success" href="#">Next</b-button>
         </b-jumbotron>
@@ -15,10 +28,17 @@
 </template>
 
 <script>
+    import _ from 'lodash'
     export default {
         props: {
             currentQuestion: Object,
             next: Function, 
+        },
+        data() {
+            return {
+                selectedIndex: null,
+                shuffledAnswers: []
+            }
         },
         computed: {
             answers() {
@@ -26,13 +46,53 @@
                answers.push(this.currentQuestion.correct_answer)
                return answers
             }
+        },
+        methods: {
+            selectAnswer(index) {
+                this.selectedIndex = index
+                console.log(index)
+            },
+            shuffleAnswers() {
+                let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+                this.shuffledAnswers = _.shuffle(answers)
+            }
+        },
+        watch: {
+            currentQuestion: {
+                immediate: true,
+                handler() {
+                    this.selectedIndex = null
+                    this.shuffleAnswers()
+                }
+            }
+            
+        },
+        mounted() {
+            this.shuffleAnswers()
         }
-
-
-        // mounted() {
-        //     console.log(this.currentQuestion)
-        // }
     }
     // any variables that we pass from App to Question, in order for it to display in the html, it must be referenced in the props
 </script>
     
+
+<style scoped> 
+    .list-group {
+        margin-bottom: 15px;
+    }
+    .list-group-item:hover {
+        background: #EEE;
+        cursor: pointer;
+    }
+    .btn {
+        margin: 0 5px;
+    }
+    .selected {
+        background-color: lightblue;
+    }
+    .correct {
+        background-color: green;
+    }
+    .incorrect {
+        background-color: red;
+    }
+</style>
